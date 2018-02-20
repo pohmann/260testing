56e8426363420dfb7fa4d33efed934f7d1c72c05
ohmannp <ohmannp@xavier.edu>
2018-02-20 11:03:33 -0500

    Universe is better than world

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 int main(){
-  cout << "Hello, world!" << endl;
+  cout << "Hello, universe!" << endl;
   cout << "Another message!" << endl;
   cout << "Yet another message!" << endl;
 }
